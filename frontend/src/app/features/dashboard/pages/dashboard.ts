ff79d52209a65b42125a0215901d10a1b3c43c77
Z-Hunt <102046927+Z-Hunt@users.noreply.github.com>
2025-10-21 21:46:30 +0200

    ✨ Mise à jour : Affichage du nombre total de clients récupérés depuis l’API

--- frontend/src/app/features/dashboard/pages/dashboard.ts
+++ frontend/src/app/features/dashboard/pages/dashboard.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TITLE } from '@core/constants';
 import { PageTitleService } from '@shared/services/page-title-service';
+import { ClientService } from '@features/clients/services/client-service';
 
 @Component({
   standalone: true,
@@ -14,6 +15,7 @@ export class Dashboard implements OnInit{
 
   private pageTitleService = inject(PageTitleService);
   private route = inject(Router);
+  protected clientService = inject(ClientService);
 
   ngOnInit(): void {
     console.log('(Dashboard - ngOnInit) Initialisation du composant...');
