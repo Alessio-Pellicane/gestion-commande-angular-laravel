9743c0618435168d8d81ad5eb3257d42e7ef013d
Z-Hunt <102046927+Z-Hunt@users.noreply.github.com>
2025-10-28 23:21:12 +0100

    :sparkles: Ajout de la page produit

--- frontend/src/app/features/produits/produits.ts
+++ frontend/src/app/features/produits/produits.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-produits',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './produits.html',
   styleUrl: './produits.scss'
 })
